06f79087e1c8c3bdf1f8da846aaad4ad2b62d8ed
Austin W <austinwoo123@gmail.com>
2021-02-02 23:09:11 -0800

    hook project with JawsDB

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -40,7 +40,7 @@ router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
 
-    burger.deleteOne(condition, function (result) {
+    burgerCon.deleteOne(condition, function (result) {
         if (result.affectedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
             return res.status(404).end();
